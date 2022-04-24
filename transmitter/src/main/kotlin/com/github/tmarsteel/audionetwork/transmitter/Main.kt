e4e5ba0570c098dcabec1eb7d19f011b6c0b4088
Tobias Marstaller <tobias.marstaller@gmail.com>
2022-04-24 18:44:58 +0200

    transmitter: add OpusEncodingOutputStream

--- transmitter/src/main/kotlin/com/github/tmarsteel/audionetwork/transmitter/Main.kt
+++ transmitter/src/main/kotlin/com/github/tmarsteel/audionetwork/transmitter/Main.kt
@@ -1,62 +1,129 @@
 package com.github.tmarsteel.audionetwork.transmitter
 
+import club.minnced.opus.util.OpusLibrary
 import com.github.tmarsteel.audionetwork.protocol.AudioData
 import com.github.tmarsteel.audionetwork.protocol.AudioReceiverAnnouncement
 import com.google.protobuf.ByteString
 import com.google.protobuf.InvalidProtocolBufferException
+import tomp2p.opuswrapper.Opus
 import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.OutputStream
 import java.net.DatagramPacket
 import java.net.DatagramSocket
 import java.net.InetAddress
 import java.net.InetSocketAddress
 import java.net.Socket
 import java.nio.ByteBuffer
-import java.time.Duration
+import java.nio.IntBuffer
+import java.nio.ShortBuffer
+import java.nio.file.Paths
 import javax.sound.sampled.AudioFormat
 import javax.sound.sampled.AudioSystem
 import kotlin.concurrent.thread
+import kotlin.io.path.readText
 
 fun main(args: Array<String>) {
+    OpusLibrary.loadFromJar()
+    println(Opus.INSTANCE.opus_get_version_string())
     tx(File(args[0]))
 }
 
-fun tx(file: File) {
-    val audioFormat = AudioSystem.getAudioFileFormat(file).format
-    require(audioFormat.channels == 2)
-    require(audioFormat.sampleSizeInBits == 16)
-    val audioIn = AudioSystem.getAudioInputStream(file)
-    BroadcastReceiver(InetSocketAddress(58765)) { announcementMessage, deviceIp ->
-        println("Found receiver ${announcementMessage.deviceName} $deviceIp, starting tx")
-        val txSocket = Socket(deviceIp, 58764)
-        val buffer = ByteArray(audioFormat.floorToNextMultipleOfFrameSize(8192))
-        val bufferDurationMillis = audioFormat.getBufferDurationInMillis(buffer.size)
-        do {
-            val nBytesRead = audioIn.readNBytes(buffer, 0, buffer.size)
-            val message = AudioData.newBuilder()
-                .setBytesPerSample(2)
-                .setSampleRate(audioFormat.sampleRate.toInt())
-                .setSamples(ByteString.copyFrom(buffer, 0, nBytesRead))
-                .build()
-            message.writeDelimitedTo(txSocket.getOutputStream())
-            txSocket.getOutputStream().flush()
-            Thread.sleep(bufferDurationMillis)
-        } while (audioIn.available() > 0)
-        txSocket.close()
-        println("Tx done")
+fun recode() {
+    val wavHeader = ByteArray(0x2C)
+    FileInputStream("C:\\Users\\tobia\\Desktop\\EMOTIONAL DAMAGE 48khz.wav").use {
+        it.read(wavHeader)
+    }
+
+    val monitoredData = Paths.get("C:\\Users\\tobia\\Desktop\\com4_output.log").readText()
+        .replace("\n", "")
+        .replace("\r", "")
+        .replace(" ", "")
+        .windowed(2, 2) { Integer.parseInt(it.toString(), 16).toByte() }
+        .toByteArray()
+
+    FileOutputStream("C:\\Users\\tobia\\Desktop\\received.wav").use { out ->
+        out.write(wavHeader)
+        out.write(monitoredData)
     }
 }
 
-fun AudioFormat.createBufferOfDuration(forDuration: Duration): ByteArray {
-    val size = Math.toIntExact(sampleRate.toLong() * forDuration.toNanos() / 1000000000L * (sampleSizeInBits / 8).toLong() * channels.toLong())
-    return ByteArray(size)
+fun recode2() {
+    val file = File("C:\\Users\\tobia\\Desktop\\EMOTIONAL DAMAGE 48khz.wav")
+    val sourceFormat = AudioSystem.getAudioFileFormat(file).format
+    val wavHeader = ByteArray(0x2C)
+    val encodedFrames = mutableListOf<ByteBuffer>()
+
+    FileInputStream(file).use { sourceIn ->
+        sourceIn.read(wavHeader)
+
+        val opusOut = OpusEncodingOutputStream(
+            sourceFormat,
+            object : OpusEncodingOutputStream.Downstream {
+                override fun onEncodedFrameAvailable(data: ByteBuffer) {
+                    encodedFrames.add(data)
+                }
+
+                override fun close() {}
+            },
+        )
+        sourceIn.copyTo(opusOut)
+    }
+
+    val outputBuffer = ShortBuffer.allocate(4096)
+    val opusError = IntBuffer.allocate(1)
+    val opusDecoder = Opus.INSTANCE.opus_decoder_create(sourceFormat.sampleRate.toInt(), sourceFormat.channels, opusError)
+    throwOnOpusError(opusError.get())
+
+    FileOutputStream("$file.recode.wav").use { fileOut ->
+        fileOut.write(wavHeader)
+        for (encodedFrame in encodedFrames) {
+            require(encodedFrame.position() == 0)
+            outputBuffer.clear()
+            val nDecodedSamples = Opus.INSTANCE.opus_decode(
+                opusDecoder,
+                encodedFrame.array(),
+                encodedFrame.limit(),
+                outputBuffer,
+                outputBuffer.capacity() / 2,
+                0
+            )
+            if (nDecodedSamples < 0) {
+                throwOnOpusError(nDecodedSamples)
+            }
+
+            outputBuffer.position(0)
+            outputBuffer.limit(nDecodedSamples * sourceFormat.channels)
+            while (outputBuffer.hasRemaining()) {
+                val short = outputBuffer.get()
+                val firstByte = short.toInt() and 0xFF
+                val secondByte = (short.toInt() shr 8) and 0xFF
+                fileOut.write(firstByte)
+                fileOut.write(secondByte)
+            }
+        }
+    }
 }
 
-fun AudioFormat.floorToNextMultipleOfFrameSize(byteCount: Int): Int {
-    return (byteCount / frameSize) * frameSize
-}
+fun tx(file: File) {
+    BroadcastReceiver(InetSocketAddress(58765)) { announcementMessage, deviceIp ->
+        println("Found receiver ${announcementMessage.deviceName} $deviceIp, starting tx")
+        val txSocket = Socket(deviceIp, 58764)
 
-fun AudioFormat.getBufferDurationInMillis(bufferSize: Int): Long {
-    return (bufferSize.toLong() * 1000_000_000L / frameSize.toLong() / frameRate.toLong()) / 1000_000L
+        AudioSystem.getAudioInputStream(file).use { audioIn ->
+            OpusEncodingOutputStream(
+                audioIn.format,
+                ProtobufWrappingOpusDownstream(audioIn.format, txSocket.getOutputStream(), false),
+                signal = OpusEncodingOutputStream.Signal.MUSIC,
+                maxEncodedFrameSizeBytes = 4096
+            ).use { opusEncoderOut ->
+                audioIn.copyTo(opusEncoderOut)
+                opusEncoderOut.flush()
+                println("Done")
+            }
+        }
+    }
 }
 
 class BroadcastReceiver(
@@ -106,6 +173,28 @@ class BroadcastReceiver(
     fun stop() {
         closed = true
         thread.interrupt()
+    }
+}
+
+class ProtobufWrappingOpusDownstream(
+    private val audioFormat: AudioFormat,
+    private val outputStream: OutputStream,
+    private val closeDownstream: Boolean = true
+) : OpusEncodingOutputStream.Downstream {
+
+    override fun onEncodedFrameAvailable(data: ByteBuffer) {
+        val message = AudioData.newBuilder()
+            .setSampleRate(audioFormat.sampleRate.toInt())
+            .setOpusEncodedFrame(ByteString.copyFrom(data))
+            .build()
+        message.writeDelimitedTo(outputStream)
+        outputStream.flush()
+    }
+
+    override fun close() {
+        if (closeDownstream) {
+            outputStream.close()
+        }
     }
 }
 
